78a8f8059cc4a384f8c648f386823ed3c95306d3
khushbu kumari <90909861+Khushbu-kumari-87090@users.noreply.github.com>
2023-06-12 00:11:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       life is an adventure explore
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import Form from "./Form"
+import Login from "./checkin/login/login";
+
+function App() {
+	return (
+		<div className="App">
+			<Form />
+		</div>
+	
+);
+}
+
+export default App;
+
+
